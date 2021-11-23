ee5487ca32fde5cba37c57c8cb7e38b620ac20af
Merdi-kim <merdikimuha1@gmail.com>
2021-11-23 03:47:18 -0800

    fix minor bug in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,28 +5,14 @@ import '../styles/Mint.css'
 import NavBar from '../Components/NavBar'
 import store from '../store'
 import {Provider} from 'react-redux'
-import { useState, useEffect } from 'react'
 
 function MyApp({ Component, pageProps }) {
-  
-  const [wallet, setWallet] = useState(true)
-
-  const checkWallet = () => {
-    if (window.ethereum === 'undefined') {
-      setWallet(false)
-    }
-  }
-
-  useEffect(() => {
-    checkWallet()
-  }, [])
 
 
   return (
     <>
      <NavBar/>
-     {wallet && <Provider store={store}><Component {...pageProps} /></Provider>}
-     {!wallet && <div>Please install a wallet</div>}
+     <Provider store={store}><Component {...pageProps} /></Provider>
    
     
     </>
