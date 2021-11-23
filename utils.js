2454954492a2ea418f64eea4ac9dae467f5a953f
Merdi-kim <merdikimuha1@gmail.com>
2021-11-23 10:59:32 -0800

    deploy to mumbai testnet and get new keys

--- utils.js
+++ utils.js
@@ -1,2 +1,2 @@
-export const nftAddress = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512'
-export const appAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3'
+export const nftAddress = '0x087CB57Ca5b27ce2dEf36071194c07915e25BfBc'
+export const appAddress = '0xC8F71186c3823369caf2D904691C64F78dD15380'
